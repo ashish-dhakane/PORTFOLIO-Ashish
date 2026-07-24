64ad47d73fe2ca3da1d1a932cdfae32115bbd040
Ashish Dhakane <dhakneashish110@gmail.com>
2026-07-24 15:14:06 +0530

    Update Projects.tsx

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -55,7 +55,7 @@ const projects = [
     description:
       "Enterprise expense-management platform with multi-level approval workflows, role-based access control (RBAC), and real-time status tracking. Built with Node.js, Express.js, PostgreSQL, and JWT authentication, including secure file uploads and dynamic dashboards.",
     github: "https://github.com/ashish-dhakane/IMPREST-Maha-Metro",
-    //demo: "",
+    demo: "https://imprest-maha-metro.vercel.app/",
     image: "/images/project-imprest.jpg",
     color: "#2DE2E6",
   },
